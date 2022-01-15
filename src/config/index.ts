6c8e2a40b9f478efc9e76d38b3e3672809fba43f
arsaizdihar <arsadihar@gmail.como>
2022-01-16 00:42:36 +0700

    change port env

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
-const SERVER_PORT = process.env.SERVER_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const SERVER_TOKEN_EXPIRETIME =
   Number(process.env.SERVER_TOKEN_EXPIRETIME) || 3600;
 const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || "coolIssuer";
@@ -12,7 +12,7 @@ const SERVER_TOKEN_SECRET =
 
 const SERVER = {
   hostname: SERVER_HOSTNAME,
-  port: SERVER_PORT,
+  port: PORT,
   token: {
     expireTime: SERVER_TOKEN_EXPIRETIME,
     issuer: SERVER_TOKEN_ISSUER,
